7395de49d8ee3637e4d82e2230cf7ea7abfb8e81
Sharmistha2023 <146067377+Sharmistha2023@users.noreply.github.com>
2026-01-06 11:04:21 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import logging
 import numpy as np
-df = pd.read_csv('/mnt/in/Algerian_forest_fires_dataset_CLEANED.csv')
+df = pd.read_csv('/mnt/in/Fire_dataset_cleaned.csv')
 with open('/mnt/in/Algerian_forest_fires_dataset_CLEANED.csv', 'r') as f:
     print(f.read(500))
 logging.info(f"DF Columns: {list(df.columns)}")
@@ -9,5 +9,5 @@ print(f"df columns: {df.columns}")
 df.columns
 df.drop(['day','month','year'], axis=1, inplace=True)
 df['Classes']= np.where(df['Classes']== 'not fire',0,1)
-df.to_csv('/mnt/ou/Algerian_forest_fires_dataset_CLEANED_NEW.csv', index=False)
+df.to_csv('/mnt/ou/Fire_dataset_cleaned.csv', index=False)
 print("New CSV file saved!")
